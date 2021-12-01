44166fd30f40ee23b6e4b34e4fa42ddc2e84a95d
Chen Jin <53110673+chen-jin021@users.noreply.github.com>
2021-12-01 03:58:05 -0500

    heroku config

--- server.js
+++ server.js
@@ -46,4 +46,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
